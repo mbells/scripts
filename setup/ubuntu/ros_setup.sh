02b0a90e18eeb06826a839ec2c38361af2a0a1b5
Matthew Bells <github@mbells.ca>
2017-03-06 20:53:58 -0500

    ROS
    Add some helpers for ROS.

---
+++ setup/ubuntu/ros_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Setup ROS
+#     version Kinetic
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+
+sudo apt-get install ros-kinetic-desktop-full
+
+# Initialize rosdep package manager
+sudo rosdep init
+rosdep update
+
+sudo apt-get install python-rosinstall

78d4544e7478b38e8a2766f7a09fb8ea1a6c2c76
Matthew Bells <github@mbells.ca>
2018-12-11 22:01:19 -0500

    Updates for Ubuntu 18.04
    Also add lsnet script.

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -1,69 +1,60 @@
 #!/bin/bash
 # Installs packages that are useful for a full dev environment.
-# Currently targeted at 14.04
+# Currently targeted at 18.04
 
-apt-get install vlc browser-plugin-vlc
-apt-get install chromium-browser
-apt-get install cmake
-apt-get install cups-pdf
-apt-get install easystroke
-apt-get install firefox
-apt-get install git
-apt-get install gitk
-apt-get install indicator-multiload
-apt-get install inkscape
-apt-get install kate
-apt-get install libav-tools
-apt-get install libavcodec-extra-54
-apt-get install libavformat-extra-54
-apt-get install meld
-#apt-get install nautilus-dropbox
-apt-get install octave
-apt-get install python-matplotli
-apt-get install python-uniconvertor
-apt-get install python3-numpy
-apt-get install ubuntu-restricted-extras
-apt-get install virtualbox
-apt-get install wine
-apt-get install xtitle
+apt install vlc browser-plugin-vlc
+apt install chromium-browser
+apt install cmake
+apt install cups-pdf
+apt install easystroke
+apt install firefox
+apt install git gitk
+apt install indicator-multiload
+apt install inkscape
+apt install kate
+#apt install libav-tools
+apt install libavcodec-extra
+apt install meld
+apt install net-tools
+apt install nmap
+apt install octave
+#apt install python-uniconvertor # what is this for?
+apt install python3-matplotlib
+apt install python3-numpy
+apt install ubuntu-restricted-extras
+apt install virtualbox
+#apt install wine
+apt install xtitle
 
-# Arduino
-apt-get install arduino
-apt-get install avr-libc
-apt-get install gcc-avr
+# Arduino - must download for recent version
+#apt install arduino
+#apt install avr-libc
+#apt install gcc-avr
 # UECIDE??
 
 # OpenCV
-apt-get install libopencv-dev
-apt-get install pythonopencv
+apt install libopencv-dev
+apt install python3-opencv
 
 # Vagrant
-apt-get install vagrant
+apt install vagrant
 vagrant plugin install vagrant-vbguest
 
 # OpenSCAD
 add-apt-repository ppa:openscad/releases
-apt-get update
-apt-get install openscad
+apt update
+apt install openscad
 
 # libdvdcss
 #deb http://download.videolan.org/pub/debian/stable/ /
 #wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
-sudo apt-get install libdvdread4
+sudo apt install libdvdread4
 sudo /usr/share/doc/libdvdread4/install-css.sh
 
 # Speech
-sudo apt-get install espeak
+sudo apt install espeak
 
 # ROS
-#sudo apt-get install ros-indigo-husky-desktop
+# Don't install on main computer!
+#sudo apt install ros-indigo-husky-desktop
 
-# Dropbox
-# https://www.digitalocean.com/community/tutorials/how-to-install-dropbox-client-as-a-service-on-ubuntu-14-04
-# https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016
-sudo mkdir -p /opt/dropbox
-sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
-sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox
-sudo chmod +x /etc/init.d/dropbox
-echo 'DROPBOX_USERS="mbells"' | sudo tee --append /etc/default/dropbox
-sudo update-rc.d dropbox defaults

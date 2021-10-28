0d3951bf0c95cc2c94a3fce402906b6010392cb4
Matthew Bells <github@mbells.ca>
2021-10-27 23:05:13 -0400

    Add bash_aliases

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/devel/scripts/config/inputrc ~/.inputrc
 ln -s ~/devel/scripts/config/bashrc ~/.bashrc
+ln -s ~/devel/scripts/config/bash_aliases ~/.bash_aliases
 
 git config --global push.default simple
 gsettings set com.canonical.indicator.datetime time-format 24-hour

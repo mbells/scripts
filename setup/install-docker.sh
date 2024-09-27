590425ce074b35be226011290c666f05cd5eddf2
Matthew Bells <github@mbells.ca>
2024-09-27 17:26:31 -0400

    Update Docker

--- setup/install-docker.sh
+++ setup/install-docker.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Currently targeted at 18.04
+# Currently targeted at 24.04
+# See https://snapcraft.io/install/docker/ubuntu
 
 # Docker
 sudo snap install docker
 
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
+# Update groups for current user:
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
 
+# Test:
+#service docker start  # part of main docs, needed for snap?
+docker run hello-world

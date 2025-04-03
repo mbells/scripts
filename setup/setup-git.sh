4d3cd292bed350707845db9883dfc6d787579335
Matt Bells <mbells@ottomotors.com>
2025-04-03 16:25:00 -0400

    Add git signature info

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -9,6 +9,9 @@ sudo apt install gitk
 git config --global user.name "YOUR NAME"
 git config --global user.email "userid@example.com"
 
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/KEY.PUB
+
 git config --global init.defaultBranch main
 git config --global push.default simple
 
